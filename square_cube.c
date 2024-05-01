ad4a6eeda52ab91f6ad6923b4b7e4ca5056dfc93
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-05-01 16:15:33 +0530

    square and cube

---
+++ square_cube.c
@@ -0,0 +1,13 @@
+//Caculate square and cude for given number
+
+#include<stdio.h>
+
+int main(){
+    int n, square, cube;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    square = n*n;
+    cube = n*n*n;
+    printf("Square: %d and Cube: %d: ", square, cube);
+    return 0;
+    }
