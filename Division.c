ab1315476d035d1edcb08b1d38d884ef0485f188
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-05-01 15:03:48 +0530

    Division

---
+++ Division.c
@@ -0,0 +1,13 @@
+//Division of two numbers
+
+#include<stdio.h>
+
+int main(){
+    float a, b, c;
+    printf("Enter the value of a:");
+    scanf("%f",&a);
+    printf("Enter the value of b:");
+    scanf("%f",&b);
+    printf("Division of two numbers is %f", a / b);
+    return 0;
+    }
