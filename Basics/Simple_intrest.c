76a5b91338cdd78db812ac1e2f2c1892f06436e6
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-05-16 10:24:09 +0530

    updated

--- Basics/Simple_intrest.c
+++ Basics/Simple_intrest.c
@@ -6,7 +6,7 @@ int main(){
     float p, t, r, si;
     printf("Enter the price , time and intrest: ");
     scanf("%f%f%f",&p,&t,&r);
-    si = (p+t+r)/100.00;
+    si = (p*t*r)/100.00;
     printf("Simple Intrest is: %f ",si);
     return 0;
     }
