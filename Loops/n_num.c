1e5f058b09b2c36773fb5626475fbf84979d3206
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-07-03 18:27:12 +0530

    while loop

---
+++ Loops/n_num.c
@@ -0,0 +1,14 @@
+// print number from 1 to n
+#include<stdio.h>
+
+int main(){
+    int i=1,n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    while (i<=n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
