84c8fdfcfc26bed04054ad96e8bf841cc289c661
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-07-05 06:24:07 +0530

    Even number

---
+++ Loops/Even_num.c
@@ -0,0 +1,13 @@
+// Print first n even numbers
+#include<stdio.h>
+
+int main(){
+    int n, i=2;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    while(i<=n){
+        printf("%d\n",i);
+        i = i+2;
+    }
+    return 0;
+}
