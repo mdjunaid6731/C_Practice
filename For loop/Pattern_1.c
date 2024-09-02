eca1648c954cd70ec426053c557e591ee44550ed
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-09-02 10:18:19 +0530

    pattern 1

--- For loop/Pattern_1.c
+++ For loop/Pattern_1.c
@@ -12,8 +12,8 @@ int main(){
     int i, j, n;
     printf("Enter the value of n:");
     scanf("%d", &n);
-    for(i=1; i<=n; i++){
-        for(j=1; j<=n; j++){
+    for(i=1; i<=n; i++){  //for loop for iterating rows
+        for(j=1; j<=n; j++){  //for loop for iterating colums
             printf("*");
         }
         printf("\n");
