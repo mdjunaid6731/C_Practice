4527586b8959b963d76614c25c7db39d21a8304c
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-09-26 11:47:25 +0530

    Case control implementation

---
+++ Case Control/Interger_in_words.c
@@ -0,0 +1,43 @@
+// Print integer in words
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter number:");
+    scanf("%1d", &n);  //%1d to scan single integer
+    switch(n)
+    {
+        case 0:
+            printf("Zero");
+            break;
+        case 1:
+            printf("One");
+            break;
+        case 2:
+            printf("Two");
+            break;
+        case 3:
+            printf("Three");
+            break;
+        case 4:
+            printf("Four");
+            break;
+        case 5:
+            printf("Five");
+            break;
+        case 6:
+            printf("Six");
+            break;
+        case 7:
+            printf("Seven");
+            break;
+        case 8:
+            printf("Eight");
+            break;
+        case 9:
+            printf("Nine");
+            break;
+    }
+    return 0;
+}
