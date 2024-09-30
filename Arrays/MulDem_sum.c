44cd6f7ad0efc3604a4bfd48cd7a657654d61720
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-09-30 16:08:28 +0530

    Adding all elements in matrix

---
+++ Arrays/MulDem_sum.c
@@ -0,0 +1,21 @@
+//Sum of all elements in matrix
+#include<stdio.h>
+
+int main(){
+    int i, j, m, n, a[5][5], sum=0;
+    printf("Enter order of matrix :");
+    scanf("%d%d", &m, &n);
+    printf("Enter matrix elements.....:\n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            sum = sum + a[i][j];
+        }
+    }
+    printf("\nSum of matrix elements is : %d", sum);
+    return 0;
+}
