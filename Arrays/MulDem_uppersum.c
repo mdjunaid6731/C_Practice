8a515f2938bd64a804e32cc62a4b744c5b48982f
mdjunaid6731 <mdjunaid6731@gmail.com>
2024-09-30 16:13:36 +0530

    Adding all upper triangle elements in matrix

---
+++ Arrays/MulDem_uppersum.c
@@ -0,0 +1,22 @@
+//Sum of all upper triangle elements in matrix
+#include<stdio.h>
+
+int main(){
+    int i, j, m, n, a[5][5], sum=0;
+    printf("Enter order of matrix :");
+    scanf("%d%d", &m, &n);
+    printf("Enter matrix elements.....:\n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            if(i<=j)
+                sum = sum + a[i][j];
+        }
+    }
+    printf("\nSum of matrix elements is : %d", sum);
+    return 0;
+}
